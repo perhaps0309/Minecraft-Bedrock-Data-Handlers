1a1bc8dc1d1c9a384ebb7068d7968ab5a2a445fb
perhaps <118870356+perhaps0309@users.noreply.github.com>
2024-08-21 21:37:34 -0700

    Update PlayerData.ts
    auto-fetch equippable and inventory components, store for later use and unsubscribe on player leave for performance. needs seperate function to unsubscribe in temporary use-cases

--- extensions/PlayerData.ts
+++ extensions/PlayerData.ts
@@ -1,129 +1,134 @@
-import { world, Player, Vector3, EntityComponentTypes, EntityEquippableComponent, EquipmentSlot, Effect, EffectType, EntityEffectOptions, EffectTypes } from "@minecraft/server";
-import { EffectDataT } from "../types";
+import { world, Player, Vector3, EntityComponentTypes, EntityEquippableComponent, EquipmentSlot, Effect, EffectType, EntityEffectOptions, EffectTypes, EntityInventoryComponent, EntityLoadAfterEvent, ItemStack, PlayerLeaveAfterEvent } from "@minecraft/server";
+import { safeJsonParser, safeJsonStringify } from "../functions/json";
+
+function debugWarn(functionName: string, message: string, errorStack?: string) {
+    console.warn(`[PlayerData.${functionName}] ${message} [${errorStack || "No stack provided."}]`);
+}
 
 export class PlayerData {
     Player: Player;
 
+    public playerAlive: boolean = true;
+    public InventoryComponent: EntityInventoryComponent | undefined;
+    public EquippableComponent: EntityEquippableComponent | undefined;
     constructor(player: Player) {
         this.Player = player;
-    }
+        this.InventoryComponent = this.Player.getComponent("minecraft:inventory") as EntityInventoryComponent | undefined;
+        this.EquippableComponent = this.Player.getComponent(EntityComponentTypes.Equippable) as EntityEquippableComponent | undefined;
 
-    // Vanilla effects
-
-    /**
-     * Get a players effects
-     * @returns {Effect[]} The players effects
-     * @example
-     * const playerData = new PlayerData(player);
-     * const effects = playerData.getEffects();
-     * console.log(effects); // [Effect, Effect, Effect]
-     */
-    public getEffects(): Effect[] {
-        return this.Player.getEffects();
-    }
-
-    /**
-     * Add an effect to a player
-     * @param {Effect} effect The effect to add
-     * @example
-     * const playerData = new PlayerData(player);
-     * let allEffects = EffectTypes.getAll();
-     * this.addEffect(allEffects[0], 20, { // Effect lasts for 20 ticks (1 second)
-            amplifier: 1,
-            showParticles: true
-        });
-     */
-    public addEffect(effectType: EffectType | string, duration: number, options?: EntityEffectOptions) {
-        this.Player.addEffect(effectType, duration, options);
-    }
-
-    // Custom effects with actionbar
-    /**
-     * Get custom effects
-     * @returns { [key: string]: EffectDataT } The players custom effects
-     */
-    public getCustomEffects(): { [key: string]: EffectDataT } {
-        const effects = (this.Player.getDynamicProperty("effects") as string) || "{}";
-        return safeJsonParser(effects) as unknown as { [key: string]: EffectDataT } || {};
-    }
-
-    /**
-     * Add a custom effect to a player
-     * @param {string} effectName The name of the effect
-     * @param {EffectDataT} effectData The data of the effect
-     * @example
-     * const playerData = new PlayerData(player);
-     * playerData.addCustomEffect("speed", {
-            duration: 20,
-            title: "Title",
-            effect: "Effect",
-            strength: 1,
-            startTime: 0,
-            displaying: false,
-            applied: false,
-            lastDisplayTime: 0
+        const playerSpawn = (event: EntityLoadAfterEvent) => {
+            if (event.entity === this.Player) {
+                this.InventoryComponent = this.Player.getComponent("minecraft:inventory") as EntityInventoryComponent;
+                this.EquippableComponent = this.Player.getComponent(EntityComponentTypes.Equippable) as EntityEquippableComponent;
+            }
         }
-     */
-    public addCustomEffect(effectName: string, effectData: EffectDataT) {
-        const effects = this.getCustomEffects();
-        effects[effectName] = effectData;
-        this.Player.setDynamicProperty("effects", safeJsonStringify(effects));
+
+        const playerLeave = (event: PlayerLeaveAfterEvent) => {
+            if (event.playerId === this.Player.id) {
+                this.playerAlive = false;
+                world.afterEvents.entitySpawn.unsubscribe(playerSpawn);
+                world.afterEvents.playerLeave.unsubscribe(playerLeave);
+            }
+        }
+
+        world.afterEvents.entitySpawn.subscribe(playerSpawn);
+        world.afterEvents.playerLeave.subscribe(playerLeave);
+    }
+
+    public getDynamicProperty(key: string): any {
+        return safeJsonParser(this.Player.getDynamicProperty(key));
+    }
+
+    public setDynamicProperty(key: string, value: any) {
+        if (value === undefined) throw Error("Invalid value.");
+        if (typeof value === "object") value = safeJsonStringify(value);
+
+        this.Player.setDynamicProperty(key, value);
     }
 
     /**
-     * Remove a custom effect from a player
-     * @param {string} effectName The name of the effect
+     * @returns {ItemStack[] | void} Returns the player's inventory as an array of ItemStacks, or undefined if the InventoryComponent is not found.
+     */
+    public getInventory(): ItemStack[] | void {
+        if (!this.InventoryComponent) return debugWarn("getInventory", "failed to fetch InventoryComponent", new Error().stack);
+        if (!this.InventoryComponent.container) return debugWarn("getInventory", "failed to fetch InventoryComponent.container", new Error().stack);
+        
+        let inventoryContainer = this.InventoryComponent.container;
+        let inventory: ItemStack[] = [];
+        for (let i = 0; i < inventoryContainer.size; i++) {
+            let currentItem = inventoryContainer.getItem(i);
+            if (currentItem) inventory.push(currentItem);
+        }
+
+        return inventory;
+    }
+
+    /**
+     * Adds an item to the player's inventory.
+     * @returns {ItemStack | void} Returns true if the item was successfully added to the player's inventory, otherwise false.
      * @example
-     * const playerData = new PlayerData(player);
-     * playerData.removeCustomEffect("speed");
+     * let diamondPickaxe = new ItemStack("minecraft:diamond_pickaxe", 1);
+     * let itemData = new ItemData(diamondPickaxe, player);
+     * itemData.addCustomEnchantment("fortune2", 3); // Uses EnchantmentTitles
      */
-    public removeCustomEffect(effectName: string) {
-        const effects = this.getCustomEffects();
-        delete effects[effectName];
-        this.Player.setDynamicProperty("effects", safeJsonStringify(effects));
+    public addItem(itemStack: ItemStack): ItemStack | void {
+        if (!this.InventoryComponent) return debugWarn("addItem", "failed to fetch InventoryComponent", new Error().stack);
+        if (!this.InventoryComponent.container) return debugWarn("addItem", "failed to fetch InventoryComponent.container", new Error().stack);
+
+        return this.InventoryComponent.container.addItem(itemStack);
+    }
+
+    public getEquippable() { // TODO: Implement properly refreshing the EquippableComponent if needed(need to check in game, if it needs to be updated after death)
+        if (!this.EquippableComponent) {
+            debugWarn("getEquippable", "failed to fetch EquippableComponent", new Error().stack);
+            return;
+        }
+
+        return this.Player.getComponent(EntityComponentTypes.Equippable) as EntityEquippableComponent; 
+    }
+
+    public getArmor() {
+        let equippable = this.getEquippable();
+        if (!equippable) return debugWarn("getArmor", "failed to fetch EquippableComponent", new Error().stack);
+
+        return {
+            "Head": equippable.getEquipment(EquipmentSlot.Chest),
+            "Chest": equippable.getEquipment(EquipmentSlot.Chest),
+            "Legs": equippable.getEquipment(EquipmentSlot.Legs),
+            "Feet": equippable.getEquipment(EquipmentSlot.Feet)
+        };
+    }
+
+    public getMainhand() {
+        let equippable = this.getEquippable();
+        if (!equippable) return debugWarn("getMainhand", "failed to fetch EquippableComponent", new Error().stack);
+
+        return equippable.getEquipment(EquipmentSlot.Mainhand);
     }
 
     /**
-     * Remove all custom effects from a player
+     * Searches the players inventory for an ItemStack and returns the index of the item if found.
+     * @param itemStack The ItemStack to search for in the player's inventory.
+     * @returns {number | void} Returns the index of the item in the player's inventory, or undefined if the item was not found.
      * @example
-     * const playerData = new PlayerData(player);
-     * playerData.removeAllCustomEffects();
+     * let diamondIndex = playerData.findItemStack(new ItemStack("minecraft:diamond", 64));
+     * if (diamondIndex) { // Player has an ItemStack of 64 diamonds in their inventory
+     *   debugWarn("findItemStack", "Player has 64 diamonds in their inventory.");
+     * }
      */
-    public removeAllCustomEffects() {
-        this.Player.setDynamicProperty("effects", "{}");
-    }
+    public findItemStack(itemStack: ItemStack): ItemStack | void { 
+        let playerInventory = this.getInventory();
+        if (!playerInventory) return debugWarn("findItemStack", "failed to fetch player inventory", new Error().stack);
 
-    /**
-     * Get the effect index of a player
-     * @returns {number} The effect index
-     */
-    public getEffectIndex() {
-        return this.Player.getDynamicProperty("effectIndex") as number || 0;
-    }
+        let foundItemStack = undefined;
+        for (let i = 0; i < playerInventory.length; i++) {
+            let currentItem = playerInventory[i];
+            if (currentItem.nameTag === itemStack.nameTag && currentItem.amount === itemStack.amount) {
+                foundItemStack = currentItem;
+                break;
+            }
+        }
 
-    /**
-     * Set the effect index of a player
-     * @param {number} newValue The new value of the effect index
-     */
-    public setEffectIndex(newValue: number) {
-        this.Player.setDynamicProperty("effectIndex", newValue);
+        return foundItemStack;
     }
-}
-
-// Tries to parse the value as JSON data, and if it it fails, then returns the original data.
-export function safeJsonParser(value: string | boolean | number | Vector3 | undefined) {
-    try {
-        value = JSON.parse(value as string);
-    } catch (err) { }
-    return value;
-}
-
-// Tries to stringify JSON data, and if it fails, then returns the original data.
-export function safeJsonStringify(value: any) {
-    // If the value is an object, then stringify it.
-    // Otherwise, return the original value.
-    if (typeof value === "object") {
-        value = JSON.stringify(value)
-    }
-    return value;
 }
